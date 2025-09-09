fbf663ad8719ae7dfaef932ad80d2b4793bc7e08
Maks Zakharov <mtsovik@bk.ru>
2025-09-09 22:38:34 +0500

    Настроена админка: регистрация моделей recipes и users, поиск, фильтры, inline

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,1 +1,74 @@
-# Register your models here.
+from django.contrib import admin
+from recipes.models import (
+    Tag,
+    Ingredient,
+    Recipe,
+    IngredientAmount,
+    Favorite,
+    ShoppingCart,
+)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Админка для модели Tag."""
+
+    list_display = ("id", "name", "color", "slug")
+    search_fields = ("name", "slug")
+    ordering = ("name",)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """Админка для модели Ingredient."""
+
+    list_display = ("id", "name", "measurement_unit")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+class IngredientAmountInline(admin.TabularInline):
+    """Инлайн-редактор ингредиентов внутри рецепта."""
+
+    model = IngredientAmount
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """Админка для модели Recipe."""
+
+    list_display = (
+        "id",
+        "name",
+        "author",
+        "cooking_time",
+        "pub_date",
+        "favorites_count",
+    )
+    search_fields = ("name", "author__email")
+    list_filter = ("tags",)
+    ordering = ("-pub_date",)
+    inlines = (IngredientAmountInline,)
+
+    def favorites_count(self, obj):
+        """Количество добавлений рецепта в избранное."""
+        return obj.favorites.count()
+
+    favorites_count.short_description = "В избранном"
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    """Админка для модели Favorite."""
+
+    list_display = ("id", "user", "recipe")
+    search_fields = ("user__email", "recipe__name")
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    """Админка для модели ShoppingCart."""
+
+    list_display = ("id", "user", "recipe")
+    search_fields = ("user__email", "recipe__name")
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Админка для пользователей."""
+
+    list_display = ("id", "username", "email", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "email")
+    list_filter = ("is_staff", "is_superuser")
+    ordering = ("id",)
