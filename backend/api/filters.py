d52730941e7a21baa11cf04eb5cd9611b39a70c6
Maks Zakharov <mtsovik@bk.ru>
2025-09-12 16:30:54 +0500

    Реализована фильтрация рецептов по параметрам is_favorited и is_in_shopping_cart согласно ТЗ.

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,15 +1,33 @@
 import django_filters
-from recipes.models import Recipe, Tag
+from recipes.models import Recipe
 
 
 class RecipeFilter(django_filters.FilterSet):
-    author = django_filters.NumberFilter(field_name="author__id")
-    tags = django_filters.ModelMultipleChoiceFilter(
-        field_name="tags__slug",
-        to_field_name="slug",
-        queryset=Tag.objects.all(),
+    is_favorited = django_filters.NumberFilter(method="filter_is_favorited")
+    is_in_shopping_cart = django_filters.NumberFilter(
+        method="filter_is_in_shopping_cart"
     )
 
     class Meta:
         model = Recipe
-        fields = ["author", "tags"]
+        fields = ("tags", "author")
+
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if not user.is_authenticated:
+            return queryset
+        if value == 1:
+            return queryset.filter(favorites__user=user)
+        if value == 0:
+            return queryset.exclude(favorites__user=user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if not user.is_authenticated:
+            return queryset
+        if value == 1:
+            return queryset.filter(shopping_cart__user=user)
+        if value == 0:
+            return queryset.exclude(shopping_cart__user=user)
+        return queryset
