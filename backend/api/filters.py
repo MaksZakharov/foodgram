f2c7e428a247aa71535f61099e4e12d4c5f36e84
Maks Zakharov <mtsovik@bk.ru>
2025-09-12 22:04:16 +0500

    Добавлена фильтрация рецептов по тегам (tags__slug)

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -3,6 +3,7 @@ from recipes.models import Recipe
 
 
 class RecipeFilter(django_filters.FilterSet):
+    tags = django_filters.AllValuesMultipleFilter(field_name="tags__slug")
     is_favorited = django_filters.NumberFilter(method="filter_is_favorited")
     is_in_shopping_cart = django_filters.NumberFilter(
         method="filter_is_in_shopping_cart"
