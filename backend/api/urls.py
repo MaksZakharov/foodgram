0a6ab93df19d2753a16d2ac01d1b9c1347f60a6e
Maks Zakharov <mtsovik@bk.ru>
2025-09-30 10:03:53 +0500

    fix api.urls.py and foodgram_backend.urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,34 @@
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
+from users.views import UserViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Foodgram API',
+        default_version='v1',
+        description='Документация API для проекта Foodgram',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
+router.register('users', UserViewSet, basename='users')
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagViewSet, basename='tags')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
+    path(
+        'docs/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
 ]
--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -2,35 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-from rest_framework.routers import DefaultRouter
-from users.views import UserViewSet
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title='Foodgram API',
-        default_version='v1',
-        description='Документация API для проекта Foodgram',
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-router = DefaultRouter()
-router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
     path('api/', include('api.urls')),
-    path('api/auth/', include('djoser.urls.authtoken')),
-    path(
-        'api/docs/',
-        schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc',
-    ),
 ]
 
 if settings.DEBUG:
